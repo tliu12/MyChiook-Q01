7ca56550ad42b5ab2b438cce7ee1c3cbe97f33db
tliu12 <tliu12@studentmailnait.ca>
2017-05-14 18:40:06 -0700

    End of Project Setup

---
+++ ChiookSolution/ChiookSystem/DAL/ChiookContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additiona1 Namespaces
+using System.Data.Entity;
+#endregion
+namespace ChiookSystem.DAL
+{
+    //This is an internal class for secuity reasons
+    //Access is restricted to within this class library project
+    //Inher DbContext for Entity Framework ,which requires
+    //   System.Data.Entity
+    internal class ChiookContext :DbContext
+    {
+        //Pass the connection string name to the 
+        //   DbContext using :base()
+        //   i.e. connection string name is "ChiookDB"
+        public ChiookContext() : base("ChiookDB")
+        {
+
+        }
+    }
+}
